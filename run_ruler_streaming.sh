a1c5d37860da43eca20b359a3c88dea2a050f0c3
Zhifan Ye <yezhifan@mail.ustc.edu.cn>
2025-03-30 04:52:58 -0400

    add a script for running streaming-llm with RULER tasks

---
+++ run_ruler_streaming.sh
@@ -0,0 +1,30 @@
+bash scripts/run_streaming.sh longchat-7b-32k-streaming synthetic
+
+# Adjust streaming-llm hyperparameters in scripts/run_streaming.sh:
+# 
+# export RECENT_SIZE_RATIO=?
+# export START_SIZE=?
+
+
+# Adjust test task length in scripts/config_models.sh:
+# 
+# SEQ_LENGTHS=(
+#     # 65536
+#     # 57344
+#     # 49152
+#     # 40960
+#     # 32768
+#     # 24576
+#     # 16384
+#     # 8192
+#     4096
+# )
+
+# Adjust test task sample numbers in scripts/config_tasks.sh:
+# 
+# NUM_SAMPLES=?
+
+
+# Adjust streaming-llm model architecture in scripts/pred/model_wrappers.py:
+# 
+# class StreamingLongChatModel:
